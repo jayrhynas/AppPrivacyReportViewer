363e6f2f15db9f534888357069892bd2a4b24359
Jayson Rhynas <jayrhynas@gmail.com>
2021-09-22 16:08:50 -0400

    Initial commit

---
+++ Shared/AppPrivacyReportViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPrivacyReportViewerApp.swift
+//  Shared
+//
+//  Created by Jayson Rhynas on 2021-09-22.
+//
+
+import SwiftUI
+
+@main
+struct AppPrivacyReportViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
